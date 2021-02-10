19e1b01cc22c6e6d7f59013a7ff75c24fd034b4c
Ezra Buehler <ezra@easyb.ch>
2021-02-10 10:57:36 +0100

    Add hello world code

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
+use nb;
+use stm32f0xx_hal::{pac, prelude::*, serial::Serial};
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    if let Some(p) = pac::Peripherals::take() {
+        let mut flash = p.FLASH;
+        let mut rcc = p.RCC.configure().sysclk(48.mhz()).freeze(&mut flash);
+
+        let gpioa = p.GPIOA.split(&mut rcc);
+
+        let (tx, rx) = cortex_m::interrupt::free(move |cs| {
+            (
+                gpioa.pa2.into_alternate_af1(cs),
+                gpioa.pa3.into_alternate_af1(cs),
+            )
+        });
+
+        let mut serial = Serial::usart2(p.USART2, (tx, rx), 115_200.bps(), &mut rcc);
+
+        for byte in b"Hello, world!\r\n" {
+            nb::block!(serial.write(*byte)).unwrap();
+        }
+
+        loop {
+            let received = nb::block!(serial.read()).unwrap();
+            nb::block!(serial.write(received)).ok();
+        }
+    }
 
     loop {
-        // your code goes here
+        continue;
     }
 }
